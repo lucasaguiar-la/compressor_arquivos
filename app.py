447d4537068e04364826d7085fade351a51b0061
Lucas Aguiar <lucas.aguiar.ads@gmail.com>
2024-02-29 13:39:52 -0300

    feat: Formatação KBs e MBs

--- app.py
+++ app.py
@@ -5,20 +5,14 @@ from PIL import Image
 main_path = os.path.dirname(os.path.abspath(__file__))
 output_path = os.path.join(main_path, "arquivos")
 
-def format_sizes(size):
-    if size < 1024:
-        return f"{size:.3f} KB" 
-    else:    
-        return f"{size/1024:.2f} MB"
-
 def reduce_img(size, quality=10):
-    print(f"Arquivo:{filename} | Tamanho original: {format_sizes(size)}")
-    sleep(1)
+    print(f"\nArquivo:{filename} | Tamanho original: {size / 1024:.2f} MB")
     file_path = os.path.join(output_path, filename)
     img = Image.open(file_path)
     img.save(file_path,quality=quality)
     size = os.path.getsize(output_path+"\\"+filename)
-    print(f"Arquivo reduzido com sucesso!\nTamanho final: {format_sizes(size)}")
+    print(f"Arquivo reduzido com sucesso!\nTamanho final: {size / 1024:.2f} KB")
+    sleep(1)
 
 print("Procurando arquivos para compressão...")
 sleep(1)
